f973d2f159ae58e6bd1f43a341de8ec5855f6f58
richard <richard@eighty-five.net>
2019-02-02 14:43:42 -0800

    remove asciimatics

--- asciimatics_bar.py
+++ asciimatics_bar.py
@@ -1,51 +1,9 @@
-from asciimatics.effects import Print
-from asciimatics.renderers import BarChart, FigletText
-from asciimatics.scene import Scene
-from asciimatics.screen import Screen
-from asciimatics.exceptions import ResizeScreenError
 import sys
-import math
-import time
-from random import randint
 import mido
-import sys
 import pickle
 import json
 
 MIDI_CC_NUMS = range(12,28)
-
-def getCCValFuncs():
-    funcs = []
-    for control in MIDI_CC_NUMS:
-       # print(control)
-        if patch.keyExists((patch.getCurrentChannel(),control)):
-            print("bingo")
-            funcs.append(lambda: patch.values[patch.getCurrentChannel(),control])
-        else:
-            funcs.append(lambda: 0)
-    return funcs
-
-def demo(screen):
-    scenes = []
-    
-    effects = [
-        Print(screen,
-              BarChart(
-                      18, 64,
-                      getCCValFuncs(),
-                      colour=[c for c in range(1, 8)],
-                      bg=[c for c in range(1, 8)],
-                      scale=128.0,
-                      axes=BarChart.X_AXIS,
-                      intervals=8,
-                      labels=True,
-                      border=False),
-                  x=2, y=3, transparent=False, speed=2)
-    ]
-
-    scenes.append(Scene(effects, -1))
-    screen.play(scenes, stop_on_resize=True)
-
 
 class CCPatch:
     lastMessage = None
@@ -81,11 +39,8 @@ class CCPatch:
 
     def save(self):
         with open('deviceinfo.json', 'w') as f:
-            #pickle.dump(self.deviceInfo, f)
-
             json.dump(self.deviceInfo, f)
         print('Done!')
-
 
     def onMessage(self, name, message):
         if message.type != 'control_change': return 
@@ -97,12 +52,3 @@ class CCPatch:
 
 patch = CCPatch()
 patch.configure()
-
-while True:
-    #print(patch.values)
-    #x = 1
-    try:
-        Screen.wrapper(demo)
-        sys.exit(0)
-    except ResizeScreenError:
-        pass
