5e99635c038325272cf2799cb4f10e89cbb7fbad
richard <richard@eighty-five.net>
2019-02-24 14:33:10 -0800

    Rewrite compareSysex() to cope with returned values. Retrieve midi channel from controller on init.

--- ccpatch.py
+++ ccpatch.py
@@ -10,7 +10,6 @@ import time
 import re
 
 # TODO: Progress bar and tracking of synced encoders
-# TODO: Configure recall and store buttons to -/+ the global midi channel
 # TODO: recall and store buttons should, if there are values for that channel,
 #       lock encoders to new values, request user input on encoders, show progress bar.  
 # TODO: As soon as all values are synced unlock all encoders
@@ -49,18 +48,21 @@ class CCPatch:
     sysexListeners = {}
     ccListeners = {}
 
-#   MMC MODE (VV=7)
-#   Invoked by F0 00 20 6B 7F 42 02 00 01 cc 07 F7.
-
     def configure(self):
         mido.set_backend('mido.backends.rtmidi')
         self.connectController()
         self.connectInstrument()
 
+        # f0 00 20 6b 7f 42 02 00 40 06 08 f7 
+        # Listen for current global chan which will be requested next
+        self.addSysexListener((0xF0,0x00, 0x20, 0x6b, 0x7F, 0x42, 0x02, 0x00, 0x40, 0x06, 0xF7),self.setCurChan)
+        hexGetGlobalChan = [0x00, 0x20, 0x6B, 0x7F, 0x42, 0x01, 0x00, 0x40, 0x06]
+        self.sendSysexToController(hexGetGlobalChan)
+
         # Beatstep transport stop
-        self.addSysexListener((0xF0,0x7F,0x7F,0x06,0x01,0xF7), self.save)
+        #self.addSysexListener((0xF0,0x7F,0x7F,0x06,0x01,0xF7), self.save)
         # Beatstep transport play
-        self.addSysexListener((0xF0,0x7F,0x7F,0x06,0x02,0xF7), self.getUserTweakage)
+        #self.addSysexListener((0xF0,0x7F,0x7F,0x06,0x02,0xF7), self.getUserTweakage)
 
         # Listen for CC 0x34 from recall button to decrement,
         # set global midi channel, and then get some user tweaks to sync
@@ -87,7 +89,7 @@ class CCPatch:
         hexSetStoreMMCx35 = [0x00, 0x20, 0x6B, 0x7F, 0x42, 0x02, 0x00, 0x03, 0x5D, 0x35]
         self.sendSysexToController(hexSetStoreMMCx35)
 
-        hexGetGlobalChan = [0x00, 0x20, 0x6B, 0x7F, 0x42, 0x02, 0x00, 0x40, 0x06, self.curChan]
+        hexGetGlobalChan = [0x00, 0x20, 0x6B, 0x7F, 0x42, 0x01, 0x00, 0x40, 0x06]
         self.sendSysexToController(hexGetGlobalChan)
         
     def sendSysexToController(self,sysex):
@@ -95,6 +97,10 @@ class CCPatch:
             self.controllerPort.send(mido.Message('sysex', data=sysex))
         except Exception as e:
             print("Error sending sysex to device")
+
+    def setCurChan(self,value) :
+        print("Setting channel to: "+str(value))
+        self.curChan = value[0]
 
     def decrementChan(self,value):
         if self.curChan > 0:
@@ -121,21 +127,34 @@ class CCPatch:
     
     # Compares sysex commands. Returns either False, or with an array of remaining unmatched bytes from sysex2
     # In the case of an exact match, returns an empty list
-    def compareSysex(self,sysex1,sysex2):
-        print(str(sysex1))
-        print(str(sysex2))
+    def compareSysex(self,sysex_listener,sysex_message):
+        # results array for return values in sysex message
         result = []
-        for i in range(0,len(sysex1)):
-            if sysex1[i] != sysex2[i]:
-                return False
-        if len(sysex2) > len(sysex1):
-            for i in range(len(sysex1),len(sysex2)):
-                result[i] = sysex2[i]
-        return result
 
-    def compareCC(self,cc1,cc2):
-        print("Comparing "+str(cc1)+" to "+str(cc2))
-        return False
+        # either sysex_listener or sysex_message could be longer than the other
+        # if the listener is longer than the message then it won't match
+        # so we may as well return false
+        if len(sysex_listener) > len(sysex_message): return False
+
+        # if we have an exact match then return an empty list for return vals
+        if sysex_listener == sysex_message: return []
+
+        # remove the trailing 0xF7 from sysex_listener, or else it may try to compare it
+        sysex_listener = list(sysex_listener)
+        sysex_listener.remove(0xF7)
+        sysex_listener = tuple(sysex_listener)
+
+        # if any bytes don't match now up to the length of sysex_listener then it's not a match
+        for i in range(0,len(sysex_listener)):
+            if sysex_listener[i] != sysex_message[i]: return False
+      
+        # loop through any remaining message bytes (except the trailing 0xF7), which don't appear in sysex_listener
+        # These are result bytes
+        for i in range(len(sysex_listener),len(sysex_message)):
+            if (sysex_message[i] != 0xF7):
+                result.append(sysex_message[i])
+
+        return result
 
     def processCCListeners(self,message):
         for cc in self.ccListeners:
@@ -229,29 +248,6 @@ class CCPatch:
                 self.freezeEncoder(encoder,value)
                 print("Encoder values locked")
 
-    def makePending(self):
-        # Copy self.values to self.pending
-        self.pending = self.values
-        return self.pending
-
-    def getPending(self):
-        if (self.pending != None):
-            return self.pending
-        else:
-            return makePending()
-
-    def unpendCC(self,channel,control):
-        print("Unpending channel: "+channel+" control:"+control)
-        #remove channel/control pair from self.pending
-
-    def getCurrentChannel(self):
-        hexGetChan = [0x00, 0x20, 0x6B, 0x7F, 0x42, 0x01, 0x00, 0x50, 0x0B]
-        try:
-            self.controllerPort.send(mido.Message('sysex', data=hexGetChan))
-        except Exception as e:
-            print("Error sending sysex to device")
-
-
     def getUserTweakage(self):
         channel = input("Which channel?:")
 #        print("Please tweak the following controllers")
@@ -306,15 +302,8 @@ class CCPatch:
             if  self.curCCMessage != self.lastCCMessage:
                 self.values[message.channel-1][message.control] = message.value
                 self.lastCCMessage = self.curCCMessage
-        # listen for save and broadcast commands from beatstep stop and play buttons
-
         elif message.type == 'sysex':         
             self.processSysexListeners(message)
-        #elif message.type == 'sysex' and message.bytes()[3]==6:
-           # if (message.bytes()[4]==1):
-           #     self.save()
-           # elif (message.bytes()[4]==2):
-           #     self.getCurrentChannel()
         else:
             print(message)
 
@@ -326,82 +315,3 @@ if len(sys.argv) > 1:
 
 while True:
     continue
-
-
-#        for channeldata in self.values.items():
-#            for controldata in channeldata[1].items():
-#                channel = int(channeldata[0])
-#                control = int(controldata[0])
-#                value = int(controldata[1])
-#                encoder = self.controlToEncoder(control)
-#
-#                setMinSyx=self.cmdBeatstep(0x02,channel,encoder,value)
-#                setMaxSyx=self.cmdBeatstep(0x02,channel,encoder,value)
-
-                # need to listen for CC messages from beatstep
-                # maybe this process should be executed in the load method
-                # yup i think it should
-
-                #while True:
-                #    for msg in self.controllerPort.iter_pending():
-                #        print(msg)
-
-                #print("\nPlease tweak encoder #: "+str(self.controlToEncoder(control)))
-
-                # We're going to need to wait and listen for CC messages
-
-#                while (self.currMessage is not None
-#                        and self.currMessage.type is not "control_change"
-#                        and self.currMessage.channel is not channel and
-#                        self.currMessage.control is not control):
-#                       x = 1
-#               print("\nThank you!")
-                #self.controllerPort.send(mido.Message('sysex', data=setMinSyx))
-                #self.controllerPort.send(mido.Message('sysex', data=setMinSyx))
-
-# Set minimum val of encoders F0 00 20 6B 7F 42 02 00 04 20 23 F7
-#        print("Broadcasting current patch...")
-#        for channeldata in self.values.items():
-#            for controldata in channeldata[1].items():
-#                self.instrumentPort.send(mido.Message('control_change', channel=int(channeldata[0]),control=int(controldata[0]),value=int(controldata[1])))
-
-                #F0 00 20 6B 7F 42 02 00 00 2x vv F7
-                #240 00, 32, 107, 127, 66, 02, 00, 00, 20, 127
-                #self.controllerPort.send(mido.Message('sysex', data=[0,32,107,127,66,2,0,0,20,127]))
-                #self.controllerPort.send(mido.Message('sysex', data=[1,2,3]))
-                #self.controllerPort.send(mido.Message.from_bytes([0xF0, 0x00, 0x20, 0x6B, 0x7F,
-                #                                 0x42, 0x02, 0x00, 000, 0x20, 0x7F, 0xF7]))
-                #F0 00 20 6B 7F 42 02 00 50 0B nn F7
-                #self.controllerPort.send(mido.Message('sysex', data=[0x00, 0x20, 0x6B, 0x7F, 0x42, 0x02, 0x00, 0x50, 0x0B, 0x04, 0xF7]))
-                #self.controllerPort.send(mido.Message('sysex', data=[0x00, 0x20, 0x6B, 0x7F, 0x42, 0x02, 0x00, 0x50, 0x0B, 0x04, 0x7F]))
-
-                #self.controllerPort.send(mido.Message('note_on', note=0, velocity=56, time=6.2))
-
-#        print("Please tweak the following controllers")
-#        for channeldata in self.values.items():
-#            for controldata in channeldata[1].items():
-#                targetChannel = int(channeldata[0])
-#                targetControl = int(controldata[0])
-#                targetValue = int(controldata[1])
-#                targetEncoder = self.controlToEncoder(targetControl)
-#                targetEncoderPosition = self.encoderToPosition(targetEncoder)
-
-    # This method needs to wait for the user to tweak each encoder associated with
-    # each channel and control stored in self.values
-    # self.values = [[chan > [control > value]]
-    # If we are not to request each encoder on each channel, in order
-    # then we need a way to track that each required [chan/controller] has been tweaked
-    # We also need to indicate to the user which [chan/contoller]s still need to be tweaked
-
-    # OK so thinking about this...
-    # the beatstep is channel-agnostic when it comes to setting encoder values
-    # so we need to set the values each time we change channel
-    # We're also going to need to tell ccpatch each time, which channel we want
-    # to sync the values for. This could be done with user input from input()
-
-    # How are we going to kick getUserTweakage off?
-    # Should the whole app just listen for some kind of midi event, and then
-    # check the last played channel? Ooh no, wait... We can query the beatstep for the
-    # current channel, and listen for the answer!
-    # The script should then validate midi cc input to make sure the user
-    # doesn't switch the channel mid-process
